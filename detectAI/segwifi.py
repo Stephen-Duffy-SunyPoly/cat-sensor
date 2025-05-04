d4a616bd9ca67f42a40bcd59983a08b565f55e79
William-Dambra <dambraw@sunypoly.edu>
2025-05-04 15:58:50 -0400

    Add files via upload
    Updated to convert rgb565

--- detectAI/segwifi.py
+++ detectAI/segwifi.py
@@ -4,83 +4,82 @@ Created on Wed Apr 16 16:25:25 2025
 
 @author: wdogw
 (With some help from our robot friend ChatGPT.)
-additional modifications made by Stephen
 """
 
 import socket
-import torch
 from transformers import pipeline
 import matplotlib.pyplot as plt
-import sys
 import PIL.Image as Image
+import numpy as np
 
 #Fix so torch and matplotlib don't **** each other.
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-#set host to 0.0.0.0 to bind to any/all interfaces
-HOST = '0.0.0.0'  # The IP address of your computer
+HOST = '192.168.254.106'  # The IP address of your computer
 PORT = 33333           # Port to listen on
 
-#YES this is necessary for google AIs
-access_token = os.getenv("HF_API_KEY") # get the API key from the environment variable
-model="google/deeplabv3_mobilenet_v2_1.0_513" #specify what AI moble to sue
+access_token = ""
+model="google/deeplabv3_mobilenet_v2_1.0_513"
 
-# Create the socket that will be used by the back end to connect to this server
+# Create the socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen(1)
 
 print(f"Listening on {HOST}:{PORT}...")
 
-#repeat this process forever
 while True:
-    #recieve an incoming connection
-    conn, addr = server_socket.accept()
     try:
         # Accept a connection
+        conn, addr = server_socket.accept()
         print(f"Connected by {addr}")
-
         buffer = b''
-        #read data fron the input stream until 921600 bytes have been read or the stream closes
-        while len(buffer) < 921600:
-            data = conn.recv(921600)  # Receive up to 6 megabytes of data (a 1080p image)
-            #print("Recieved " + str(len(data)) + " bytes.\n")
-
+        while len(buffer) < 614400:
+            data = conn.recv(614400)  # Receive up to 6 megabytes of data (a 1080p image)
+            print("Recieved " + str(len(data)) + " bytes.\n")
+        
             buffer += data
-            if not data: # if the stream closed
+            if not data:
                 break
+        
+        #print(data)    
+        print("Recieved " + str(len(buffer)) + " bytes.\n")
+            
+        rgb565 = np.frombuffer(buffer, dtype=np.uint16)
 
-        #print(data)
-        #print("Recieved " + str(len(buffer)) + " bytes.\n")
-
-        #convert the input bytes to an image
-        image = Image.frombytes('RGB', (320, 240), buffer)
-
-        #create the AI
+        # Create empty array to store RGB888 result
+        rgb888 = np.zeros((rgb565.size, 3), dtype=np.uint8)
+        
+        # Split the RGB565 value into R, G, B components
+        rgb888[:, 0] = (rgb565 >> 11) & 0x1F  # Extract Red (5 bits)
+        rgb888[:, 1] = (rgb565 >> 5) & 0x3F   # Extract Green (6 bits)
+        rgb888[:, 2] = rgb565 & 0x1F          # Extract Blue (5 bits)
+        
+        # Scale to 8-bit (RGB888) by shifting and masking
+        rgb888[:, 0] = (rgb888[:, 0] << 3) | (rgb888[:, 0] >> 2)  # Scale Red
+        rgb888[:, 1] = (rgb888[:, 1] << 2) | (rgb888[:, 1] >> 4)  # Scale Green
+        rgb888[:, 2] = (rgb888[:, 2] << 3) | (rgb888[:, 2] >> 2)  # Scale Blue
+        
+        rgb888.reshape(640, 480, 3)
+        
+        image = Image.fromarray(rgb888, "RGB")
+        
         p = pipeline("image-segmentation", use_fast=True, model=model, token=access_token)
-
-        #run the image through the AI
+        
         segments = p(image)
-
+        
         has_cat = False
-        #check what is returned from the AI for containing a cat
         for s in segments:
             plt.figure()
-            #things for saving images of what it detected
             plt.title(s['label'])
             plt.imshow(s['mask'])
-            #check if this thing is a cat
             if s['label'] == 'cat':
                 has_cat = True
         print("Image " + ("does" if has_cat else "does not") +" contain a cat.")
-        #print(has_cat)
-        #print(str(has_cat).encode())
-
-        #send the result back to the back end
-        conn.send(str(has_cat).encode())
-    except Exception as e:
-        print("an error occored")
-        print(e)
-    #close the connection
-    conn.close()
+        
+        conn.send(has_cat.to_bytes())
+        
+        conn.close()
+    except:
+        print("An error occured.")
